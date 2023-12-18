29dfb8fe175810bdc24df659122fc1206669c984
Mattazz <matthewazada101@gmail.com>
2023-12-18 16:57:14 -0500

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -16,10 +17,16 @@ fn main() {
     println!("In file {}", config.file_path); //config.<attrib> is easier to understand relationship
     hash_print(50);
 
-    // Takes the string file_path and reads the text in it
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 // Config Struct impl fn new -> Creates a new config struct
