8490ab6d8201c152a0c3fe35d30807659b286761
Mattazz <matthewazada101@gmail.com>
2023-12-18 17:05:09 -0500

    adding lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::{hash_print, Config};
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -17,38 +16,8 @@ fn main() {
     println!("In file {}", config.file_path); //config.<attrib> is easier to understand relationship
     hash_print(50);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application Error: {e}");
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
-// Config Struct impl fn new -> Creates a new config struct
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-// UTILS
-fn hash_print(num: u8) {
-    let str = "#".repeat(num as usize);
-    println!("{}", str)
 }
